8a85a2025d6e13c4dd34b29d2d50d55f6c7dcf89
Ioan CHIRIAC <ichiriac@f2f.fr>
2017-09-30 21:55:39 +0200

    firs impl

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # codemirror-linter
+
 PHP linter for CodeMirror
--- addon/lint/php-lint.js
+++ addon/lint/php-lint.js
@@ -2,19 +2,16 @@
 // Distributed under a BSD 3-Clause License : https://github.com/glayzzle/codemirror-linter/blob/master/LICENSE
 
 (function(mod) {
-  if (typeof exports == "object" && typeof module == "object") // CommonJS
-    mod(require("../../lib/codemirror"), require('php-parser'));
-  else if (typeof define == "function" && define.amd) // AMD
-    if (window.console) {
-      window.console.error("Error: php-parser does not support AMD.");
-    }
-  else // Plain browser env
-    mod(CodeMirror, phpParser);
+  if (typeof exports == "object" && typeof module == "object") {
+    mod(require("cm/lib/codemirror"), require('php-parser'));
+  } else {
+    mod(CodeMirror, require('php-parser'));
+  }
 })(function(CodeMirror, phpParser) {
   "use strict";
 
   // validate some code
-  function validator(text, options) {
+  CodeMirror.registerHelper("lint", "php", function phpLint(text, options) {
     if (phpParser) {
       try {
         phpParser.parseCode(text);
@@ -22,16 +19,13 @@
         return [{
           message: e.message,
           severity: "error",
-          from: CodeMirror.Pos(e.lineNumber - 1, e.columnNumber),
-          to: CodeMirror.Pos(e.lineNumber - 1, e.columnNumber + 1)
+          from: CodeMirror.Pos(e.lineNumber - 1, e.columnNumber - 1),
+          to: CodeMirror.Pos(e.lineNumber - 1, e.columnNumber)
         }];
       }
     } else if (window.console) {
       window.console.error("Error: php-parser not defined, CodeMirror PHP linting cannot run.");
     }
     return [];
-  }
-
-  CodeMirror.registerHelper("lint", "php", validator);
-
+  });
 });
